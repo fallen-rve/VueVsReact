6f188f2c8fede5f9dba990ff5e4750443c6a695a
Ryan Van Ess <fallen.rve@gmail.com>
2016-04-26 20:09:49 -0500

    cleans up unused files

--- assets/php/classes/page_builder.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This Page Builder file includes all Page Builder classes
- */
-
-require( DIRPATH . '/assets/php/classes/page_builder_classes/AbstractPageBuilder.php' );
-require( DIRPATH . '/assets/php/classes/page_builder_classes/AbstractPageDirector.php' );
-require( DIRPATH . '/assets/php/classes/page_builder_classes/HTMLPageBuilder.php' );
-require( DIRPATH . '/assets/php/classes/page_builder_classes/HTMLPageDirector.php' );
--- assets/php/classes/page_builder_classes/AbstractPageDirector.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 
- */
-abstract class AbstractPageDirector {
-    abstract function __construct(AbstractPageBuilder $builder_in, $page_url_in);
-    abstract function buildPage();
-    abstract function getPage();
-}
