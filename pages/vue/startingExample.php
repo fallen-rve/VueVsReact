9961a947d15f8f9b34a5bdb235265f3f50105f1c
Ryan Van Ess <fallen.rve@gmail.com>
2016-05-02 21:49:52 -0500

    adds more examples

---
+++ pages/vue/startingExample.php
@@ -0,0 +1,15 @@
+<?php 
+    // Set page information
+    $this->title('Hello World');
+    $this->description ('Hello World test for Vue.js!');
+    $this->footer_link( "pages/vue/startingExamples.js" );
+    echo '<h1>Starting Examples!</h1>';
+?>
+<!-- this is our View -->
+<div id="example-1" class="lesson-wrap">
+  Hello {{ name }}!
+</div>
+
+<div id="example-2" class="lesson-wrap">
+  <p v-if="greeting">Hello!</p>
+</div>
